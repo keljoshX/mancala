0a185ce05af6c9ff71f86884b96384c67076aced
keljoshX <keljosh911@gmail.com>
2025-02-22 11:36:03 +0100

    home-screen

--- client/src/components/ui/svgs/bubble.tsx
+++ client/src/components/ui/svgs/bubble.tsx
@@ -1,4 +1,4 @@
-export default function Bubble() {
+export default function Bubble({ color }: { color?: string }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -8,7 +8,7 @@ export default function Bubble() {
       viewBox="0 0 20 20"
     >
       <path
-        fill="#F58229"
+        fill={color || "#F58229"}
         d="M1.041 5.834a3.125 3.125 0 1 1 6.25 0 3.125 3.125 0 0 1-6.25 0M2.709 15a3.958 3.958 0 1 1 7.917 0 3.958 3.958 0 0 1-7.917 0M9.375 5.833a4.792 4.792 0 1 1 9.583 0 4.792 4.792 0 0 1-9.583 0"
       ></path>
     </svg>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -10,12 +10,20 @@ export default function Home() {
       <div className="bg-[url('./assets/home-box.png')] bg-cover bg-center bg-no-repeat w-[874px] h-[486px] flex flex-col items-center justify-center space-y-20">
         <img src={logo} alt="logo" className="w-56 h-16" />
         <div className="flex flex-col space-y-5 text-center">
-          <Link to="/lobby">
-            <button className="bg-[#1A1D25] text-[#F58229] py-2.5 px-7 rounded-full flex flex-row items-center justify-center space-x-1">
-              <Bubble />
-              <p>Go to lobby</p>
-            </button>
-          </Link>
+          <div className="flex flex-row space-x-5">
+            <Link to="/lobby">
+              <button className="bg-[#F58229] text-[#1A1D25] py-2.5 px-7 rounded-full flex flex-row items-center justify-center space-x-1">
+                <Bubble color="#1A1D25" />
+                <p>Play for real</p>
+              </button>
+            </Link>
+            <Link to="/lobby">
+              <button className="bg-[#1A1D25] text-[#F58229] py-2.5 px-7 rounded-full flex flex-row items-center justify-center space-x-1">
+                <Bubble />
+                <p>Play for free</p>
+              </button>
+            </Link>
+          </div>  
           <Link
             to="/"
             className="flex flex-row items-center justify-center space-x-1"
